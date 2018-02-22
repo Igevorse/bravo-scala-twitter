668bcf7ed7dbce0adb6371693a5913fb876700bd
igevorse <igevorse@gmail.com>
2018-02-23 00:43:08 +0300

    Return ids on creation, remove double checks, refactor

--- src/main/scala/ru/innopolis/bravo/TweetData/Tweet.scala
+++ src/main/scala/ru/innopolis/bravo/TweetData/Tweet.scala
@@ -3,7 +3,4 @@ package ru.innopolis.bravo.TweetData
 class Tweet(id:Integer, var text:String, val authorId:Integer, val submissionTime:Long){
   var usersLiked = scala.collection.mutable.Set[Int]()
   var usersDisliked = scala.collection.mutable.Set[Int]()
-
-
-
 }
